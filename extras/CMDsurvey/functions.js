b5bf09f9bd160fceefe8af2471e9718000938aba
Vincent van Leeuwen <shoockz@hotmail.com>
2020-10-21 10:30:47 +0200

    Cleaned CMD Survey about eye colors

---
+++ extras/CMDsurvey/functions.js
@@ -0,0 +1,74 @@
+let incorrectData = [];
+let correctData = [];
+
+fetch("data.json")
+.then(response => response.json())
+.then(json => cleanData(json));
+
+cleanData = (json) => {
+  // json.forEach(entry => eyeColorData.push(.toUpperCase().replace(/\s/g, "")));
+  eyeColorData = json.map(entry => entry['oogKleur']
+                                                .toUpperCase()
+                                                .replace(/\s/g, "")
+                                                .replace(".", ",")); //Replace . to , in RGB
+
+  eyeColorData.forEach(eye => {
+    if(eye.startsWith('RGB')) {
+      rgbValues = eye.match(/\(([^)]+)\)/)[1];
+
+      eye = rgbToHex( Number(rgbValues.split(",")[0]),
+                      Number(rgbValues.split(",")[1]),
+                      Number(rgbValues.split(",")[2])).toUpperCase();
+    }
+
+    if(!eye.startsWith('#') && eye.length === 6) {
+      eye = '#' + eye;
+    }
+
+    if(eye.startsWith('#') && eye.length === 7) {
+      correctData.push(eye);
+    } else {
+      incorrectData.push(eye);
+    }
+  });
+
+  console.log('incorrect data:', incorrectData);
+  console.log('correct data:', correctData);
+
+
+  addLastToFirst(correctData);
+
+};
+
+function addLastToFirst(data){
+
+
+  let lastItem = data.pop();
+  data.unshift(lastItem);
+
+  while(document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
+  }
+
+  data.forEach(color => {
+    var node = document.createElement("DIV");
+    var textNode = document.createTextNode("" + color + "");
+    node.classList.add("box");
+    node.style.backgroundColor = color;
+    node.appendChild(textNode);
+    document.body.appendChild(node);
+  });
+
+  setInterval(addLastToFirst, 5000);
+}
+
+
+
+componentToHex = (c) => {
+  var hex = c.toString(16);
+  return hex.length === 1 ? "0" + hex : hex;
+};
+
+rgbToHex = (r, g, b) => {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
