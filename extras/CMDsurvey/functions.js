5ba7f4ebf4c92bad015ce5aeede42658be54b1e8
Vincent van Leeuwen <shoockz@hotmail.com>
2020-10-21 12:50:08 +0200

    Added multiple column options to clean

--- extras/CMDsurvey/functions.js
+++ extras/CMDsurvey/functions.js
@@ -1,36 +1,38 @@
 let incorrectData = [];
 let correctData = [];
+let colorColumn = 'oogKleur';
+// or 'lievelingskleur'!
 
 fetch("data.json")
 .then(response => response.json())
-.then(json => cleanData(json));
+.then(json => cleanData(json, colorColumn));
 
-cleanData = (json) => {
+cleanData = (json, colorColumn) => {
 
-  eyeColorData = json.map(entry => entry['oogKleur']
+  let colorData = json.map(entry => entry[colorColumn]
                           .toUpperCase()
                           .replace(/\s/g, "") // Delete all spaces
                           .replace(".", ",") // Replace points to commas in RGB
                           );
 
-  eyeColorData.forEach(eye => {
+  colorData.forEach(color => {
 
-    if(eye.startsWith('RGB')) {
-      rgbValues = eye.match(/\(([^)]+)\)/)[1]; // Get everything between the parentheses
+    if(color.startsWith('RGB')) {
+      rgbValues = color.match(/\(([^)]+)\)/)[1]; // Get everything between the parentheses
 
-      eye = rgbToHex( Number(rgbValues.split(",")[0]),
+      color = rgbToHex( Number(rgbValues.split(",")[0]),
                       Number(rgbValues.split(",")[1]),
                       Number(rgbValues.split(",")[2])).toUpperCase();
     }
 
-    if(!eye.startsWith('#') && eye.length === 6) {
-      eye = '#' + eye; // Add Hashtag before each Hexcolor.
+    if(!color.startsWith('#') && color.length === 6) {
+      color = '#' + color; // Add Hashtag before each Hexcolor.
     }
 
-    if(eye.startsWith('#') && eye.length === 7) {
-      correctData.push(eye); // Add all correct Hexcolors to this array
+    if(color.startsWith('#') && color.length === 7) {
+      correctData.push(color); // Add all correct Hexcolors to this array
     } else {
-      incorrectData.push(eye); // Add all incorrect Hexcolors to this array
+      incorrectData.push(color); // Add all incorrect Hexcolors to this array
     }
 
   });
